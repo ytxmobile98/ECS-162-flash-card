952828d317ae30d5bc5476efd4b1017f2af10bd4
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-05-20 19:22:03 -0700

    do not create database in server-database.js file

--- server/server-database.js
+++ server/server-database.js
@@ -4,19 +4,6 @@ const sqlite3 = require("sqlite3").verbose();
 const fs = require("fs");
 
 const dbFile = "Flashcards.db";
-const db = new sqlite3.Database(dbFile); // this is the actual database object
+module.exports.db = new sqlite3.Database(dbFile); // this is the actual database object; will be exported for manipulating records
 
-const createTableCommand = 'CREATE TABLE Flashcards (user INT, english TEXT, chinese TEXT, seen INT, correct INT)';
-db.run(createTableCommand, tableCreationCallback);
 
-function tableCreationCallback(err) {
-	if (err) {
-		console.log("Table creation error",err);
-	}
-	else {
-		console.log("Database created");
-		db.close();
-  }
-}
-
-module.exports.db = db;

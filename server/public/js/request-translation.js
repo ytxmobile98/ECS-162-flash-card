71de6841af4aedcd3582094615fb3027bb38f1d4
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-05-20 19:15:43 -0700

    function rename for request translation

--- server/public/js/request-translation.js
+++ server/public/js/request-translation.js
@@ -1,16 +1,23 @@
 "use strict";
 
 const queryBox = document.getElementById("js-query");
+let query = queryBox.value;
 const outputBox = document.getElementById("js-output");
+let output = outputBox.value;
 
 
-function makeRequest(url) {
+function requestTranslation(str) {
+	const url = "translate?english=" + str;
+	
 	const xhr = new XMLHttpRequest();
 	xhr.overrideMimeType("text/plain");
 	xhr.open("GET", url);
 	
 	xhr.onload = function () {
 		outputBox.value = xhr.responseText;
+		// query and output are stored as two internal strings, and are updated only when a request is returned successfully
+		query = queryBox.value;
+		output = outputBox.value;
 	}
 	
 	xhr.error = function () {
@@ -25,6 +32,6 @@ queryBox.addEventListener("keyup", function (event) {
 	const ENTER_KEY_CODE = 13;
 	
 	if (event.keyCode === ENTER_KEY_CODE) {
-		makeRequest("translate?english=" + queryBox.value);
+		requestTranslation(queryBox.value);
 	}
 })

c6de2c325d7c4a2b5b3f7b17d130fef5e8510bb8
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-05-20 22:36:20 -0700

    added console log for translation

--- server/server-translate.js
+++ server/server-translate.js
@@ -1,31 +1,29 @@
 "use strict";
 
 module.exports.translate = function (request, response) {
-	const APIrequest = require('request');
 	
-	const url = request.url;
-	const query = request.query;
+	const requestQuery = request.query;
 	const sourceLang = "english";
 	
-	if (query.hasOwnProperty(sourceLang)) {
+	if (requestQuery.hasOwnProperty(sourceLang)) {
 		
 		const APIkey = "AIzaSyDN9t1YWrQ-4LqOjUO3tUWwtx6mzjBUdJk";  // ADD API KEY HERE
 		const url = "https://translation.googleapis.com/language/translate/v2?key=" + APIkey;
 		
-		let source = query[sourceLang];
+		let source = requestQuery[sourceLang];
 		const requestObject = {
 			"source": "en",
 			"target": "zh-cn",
 			"q": [source]
 		};
 		
+		const APIrequest = require('request');
 		APIrequest({ // HTTP header stuff
 			url: url,
 			method: "POST",
 			headers: {"content-type": "application/json"},
 			json: requestObject // will turn the given object into JSON
 		}, APIcallback); // callback function for API request
-		
 		
 		// callback function, called when data is received from API
 		function APIcallback(err, APIresHead, APIresBody) {
@@ -43,6 +41,7 @@ module.exports.translate = function (request, response) {
 				}
 				else {
 					let translatedText = APIresBody.data.translations[0].translatedText;
+					console.log("Source: ", source, "; Translated: ", translatedText);
 					response.send(translatedText);
 				}
 			}

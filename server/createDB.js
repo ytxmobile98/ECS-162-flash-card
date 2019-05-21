30156e8d411f86731c55b29934b540e34a1c43e9
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-05-20 22:35:15 -0700

    set default value for seen and correct counts

--- server/createDB.js
+++ server/createDB.js
@@ -4,15 +4,19 @@
 const sqlite3 = require("sqlite3").verbose();  // use sqlite
 const fs = require("fs"); // file system
 
+const tableName = "Flashcards";
 const dbFileName = "Flashcards.db";
 // makes the object that represents the database in our code
 const db = new sqlite3.Database(dbFileName);  // object, not database.
 
+module.exports.tableName = tableName;
+module.exports.dbFileName = dbFileName;
+
 // Initialize table.
 // If the table already exists, causes an error.
 // Fix the error by removing or renaming Flashcards.db
-const cmdStr = "CREATE TABLE Flashcards (user INT, english TEXT, chinese TEXT, seen INT, correct INT)";
-db.run(cmdStr,tableCreationCallback);
+const cmdStr = "CREATE TABLE " + tableName + " (user INT, english TEXT, chinese TEXT, seen INT DEFAULT 0, correct INT DEFAULT 0)";
+db.run(cmdStr, tableCreationCallback);
 
 // Always use the callback for database operations and print out any
 // error messages you get.

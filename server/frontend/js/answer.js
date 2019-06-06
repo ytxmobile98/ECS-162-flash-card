3435b238655205a1eb8f06bded0227bfc8919815
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-06-06 13:23:38 -0700

    answer page resources minor modification

--- server/frontend/js/answer.js
+++ server/frontend/js/answer.js
@@ -3,5 +3,5 @@
 const toMainPageButton = document.getElementById("js-to-main-page-button");
 toMainPageButton.addEventListener("click", function () {
 	// This will be changed in the next assignment; now it is simply a page jump
-	window.location.href = "main.html";
+	window.location.href = "add-flashcards.html";
 })
